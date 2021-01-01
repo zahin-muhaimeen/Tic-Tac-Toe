ae0acc031a7276db5a88f283de1eaf8202d25abc
zahin-muhaimeen <zahin.muhaimeen@gmail.com>
2021-01-01 21:21:19 +1000

    More effecient checking system
    This was done by changing the board layout into nested lists.
    Though this caused extra work of having to reconfigure all the functions.
    
    I also improved the `main` function so that you can play the game
    multiple times.

--- tictactoe.py
+++ tictactoe.py
@@ -1,94 +1,108 @@
 from random import randint
 
-board = ["-", "-", "-", "-", "-", "-", "-", "-", "-"]
+board = [
+    ["-", "-", "-"],
+    ["-", "-", "-"],
+    ["-", "-", "-"],
+]
+
+turns = 0
+x_turn = True
 
 
 def printing_board():
-    print("|\t{}\t|\t{}\t|\t{}\t|".format(board[0], board[1], board[2]))
-    print("|\t{}\t|\t{}\t|\t{}\t|".format(board[3], board[4], board[5]))
-    print("|\t{}\t|\t{}\t|\t{}\t|".format(board[6], board[7], board[8]))
+    for row in board:
+        print(row)
 
-def choosing_player(position):
-    if not 0 < position <= 9:
-        print("Position not on board")
-        return True
-    elif board[position - 1] != "-":
-        print("Position is occupied by {}".format(board[position - 1]))
-        return True
+
+def get_int(prompt):
+    user_in = input(prompt)
+    while not user_in.isnumeric():
+        user_in = input(prompt)
+    return int(user_in)
+
+
+def choosing(x, player_y):
+    global turns, x_turn
+    y = 2 - player_y
+
+    if x in range(3) and y in range(3):
+        if board[y][x] == "-":
+            if x_turn:
+                board[y][x] = "X"
+                x_turn = False
+                turns += 1
+            else:
+                board[y][x] = "O"
+                x_turn = True
+                turns += 1
+        else:
+            print("Position, x: {}, y: {}, is occupied by {}".format(x, player_y, board[y][x]))
     else:
-        board[position - 1] = "X"
-        return False
+        print("Position, x: {}, y: {}, is not in board".format(x, player_y))
 
 
-def choosing_computer():
-    index = randint(0, 8)
-    while board[index] != "-":
-        index = randint(0, 8)
-    else:
-        board[index] = "O"
-
-
-def checking():
-    if board[0] == board[1] == board[2] != "-":
-        if board[0] == "X":
-            return "X Won!"
-        else:
-            return "O Won!"
-    if board[3] == board[4] == board[5] != "-":
-        if board[3] == "X":
-            return "X Won!"
-        else:
-            return "O Won!"
-    if board[6] == board[7] == board[8] != "-":
-        if board[6] == "X":
-            return "X Won!"
-        else:
-            return "O Won!"
-    if board[0] == board[3] == board[6] != "-":
-        if board[0] == "X":
-            return "X Won!"
-        else:
-            return "O Won!"
-    if board[1] == board[4] == board[7] != "-":
-        if board[3] == "X":
-            return "X Won!"
-        else:
-            return "O Won!"
-    if board[2] == board[5] == board[8] != "-":
-        if board[6] == "X":
-            return "X Won!"
-        else:
-            return "O Won!"
-    if board[0] == board[4] == board[8] != "-":
-        if board[3] == "X":
-            return "X Won!"
-        else:
-            return "O Won!"
-    if board[6] == board[4] == board[2] != "-":
-        if board[6] == "X":
-            return "X Won!"
-        else:
-            return "O Won!"
-    for i in board:
-        if i == "-":
-            return None
-    return "Draw!"
+def checking(moved):
+    if turns >= 5:
+        # Check List
+        check_list = [None, None, None]
+        # Row
+        for row in board:
+            if row.count(moved) == 3:
+                return "{} Won!".format(moved)
+        # Column
+        for index in range(len(board)):
+            for pos, row in enumerate(board):
+                check_list[pos] = row[index]
+            if check_list.count(moved) == 3:
+                return "{} Won!".format(moved)
+        # Negative Diagonal
+        for index in range(len(board)):
+            for row in board:
+                check_list[index] = board[index][index]
+        if check_list.count(moved) == 3:
+            return "{} Won!".format(moved)
+        # Positive Diagonal
+        for index in range(len(board)):
+            for row in board:
+                check_list[index] = board[2 - index][index]
+        if check_list.count(moved) == 3:
+            return "{} Won!".format(moved)
+        # Draw
+        if turns == 9:
+            return "Draw!"
 
 
 def main():
-    while True:
-        printing_board()
-        while choosing_player(int(input(": "))):
-            pass
-        if checking() != None:
-            print(checking())
-            printing_board()
-            return None
-        choosing_computer()
-        if checking() != None:
-            print(checking())
-            printing_board()
-            return None
+    global turns, x_turn
 
-main()
+    play = True
+    while play:
+        while True:
+            printing_board()
+            choosing(get_int(": "), get_int(": "))
+            if checking("X") != None:
+                printing_board()
+                print(checking("X"))
+                break
+            printing_board()
+            choosing(get_int(": "), get_int(": "))
+            if checking("O") != None:
+                printing_board()
+                print(checking("O"))
+                break
+        again = input("Would you like to play again (y/n): ")
+        while again not in "y or n":
+            again = input("Please enter `y` (`yes`) or `n` (`no`): ")
+        if again.casefold() == "n":
+            play = False
+        else:
+            turns = 0
+            x_turn = True
+            for row in board:
+                for index, pos in row:
+                    row[index] = "-"
 
+
+if __name__ == '__main__':
+    main()
